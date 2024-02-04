c1887a8dc86a85af49b4c3ed8097630e2191bf62
Ignacio <ignaciosanzmesa@gmail.com>
2024-02-04 11:36:27 +0100

    botones: cerrar, vaciar y comprar añadidos

--- carrito.js
+++ carrito.js
@@ -1,60 +1,75 @@
+let carrito = {};
 
-let cart = {};
-
-
-function addToCart(product) {
-  if (cart[product]) {
-    cart[product]++;
+// Función para agregar un producto al carrito
+function agregarAlCarrito(producto) {
+  if (carrito[producto]) {
+    carrito[producto]++;
+    
   } else {
-    cart[product] = 1;
+    carrito[producto] = 1;
   }
 
-  showCart();
+  mostrarCarrito(); 
 }
 
-function showCart() {
-  let cartContent = document.getElementById("cartContent");
+// Función para mostrar el contenido del carrito
+function mostrarCarrito() {
+  let contenidoCarrito = document.getElementById("cartContent");
+  let botonCompra = document.createElement("button");
+  let botonCerrar = document.createElement("button");
+  let botonVaciar = document.createElement("button");
 
-  if (Object.keys(cart).length === 0) {
-    cartContent.innerHTML = "Tu cesta está vacía.";
-  } else {
-    let cartItems = Object.keys(cart).map(function (product) {
-      return product + " (" + cart[product] + ")";
-    });
-    cartContent.innerHTML =
-      "Tu cesta contiene: " + "<br>" + cartItems.join("<br>");
-  }
+  let itemsCarrito = Object.keys(carrito).map(function (producto) {//map() crea un nuevo array con los resultados de llamar a una función para cada elemento del array. En este caso, para cada producto en el carrito, se ejecuta la función que toma el producto como argumento.
+    return producto + " (" + carrito[producto] + ")";//toma el nombre del producto como argumento y devuelve el nombre y la cantidad del producto en el carrito
+  });
+  contenidoCarrito.innerHTML = "Tu cesta contiene: " + "<br>" + itemsCarrito.join("<br>");
+
+  botonCompra.setAttribute("id", "compra");
+  botonCerrar.setAttribute("id", "cerrarCarrito");
+  botonVaciar.setAttribute("id", "vaciarCarrito");
+
+  botonCompra.innerHTML = "Comprar";
+  botonCerrar.innerHTML = "Cerrar";
+  botonVaciar.innerHTML = "Vaciar";
+
+  contenidoCarrito.appendChild(botonCompra);  
+  contenidoCarrito.appendChild(botonCerrar);
+  contenidoCarrito.appendChild(botonVaciar);
+
+  botonCerrar.onclick = cerrarCarro;
+  botonVaciar.onclick = vaciarCarrito;
 }
 
+function cerrarCarro(){
+  let contenidoCarrito = document.getElementById('cartContent');
+  contenidoCarrito.classList.remove('cart-visible');
+}
 
-let buttons = document.querySelectorAll("button");
+function vaciarCarrito(){
+  carrito = {}; // Vaciar el objeto del carrito
+  mostrarCarrito()
+}
+// Obtener todos los botones en la página
+let botones = document.querySelectorAll("button");
 
-buttons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    let product = this.parentElement.querySelector(".card-title").textContent;
-    addToCart(product);
+
+botones.forEach(function (boton) {// el foreach recoge los elementos del array botones y los ejecuta una vez por cada elemento del array
+  boton.addEventListener("click", function () {
+    let producto = this.parentElement.querySelector(".card-title").textContent;//se obtiene el nombre del producto asociado al botón y el querySelector selecciona el elemento HTML correspondiente
+    agregarAlCarrito(producto); // Agregar el producto al carrito
   });
 });
 
-// Asegúrate de que este código esté después de la definición de addToCart() y showCart()
-
 // Función para mostrar u ocultar el contenido del carrito
-function toggleCart() {
-    let cartContent = document.getElementById('cartContent');
-    cartContent.classList.toggle('cart-visible');
+function alternarCarrito() {
+    let contenidoCarrito = document.getElementById('cartContent');
+    contenidoCarrito.classList.toggle('cart-visible');//classList devulve un objeto con las clases de un elemento y el toggle cambia entre las clases
   }
   
-  // Añadir evento de clic al icono de la cesta
-  document.getElementById('cartIcon').addEventListener('click', function(event) {
-    event.stopPropagation(); // Evitar que el clic se propague al contenedor principal
-    toggleCart(); // Mostrar u ocultar el contenido del carrito
-  });
-  
-  // Añadir evento de clic al documento para ocultar el contenido del carrito si se hace clic fuera de él
-  document.addEventListener('click', function(event) {
-    let cartContent = document.getElementById('cartContent');
-    if (!event.target.closest('#cartContent') && !event.target.closest('#cartIcon')) {
-      cartContent.classList.remove('cart-visible');
-    }
-  });
-  
+  // Agregar un escucha de evento de clic al ícono del carrito
+document.getElementById('cartIcon').addEventListener('click', function(event) {
+  event.stopPropagation(); // Evitar que el clic se propague
+  alternarCarrito(); // Mostrar u ocultar el contenido del carrito
+});
+
+
